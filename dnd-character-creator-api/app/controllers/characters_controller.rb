83e0d1217625769989888157476ec7d085a40d81
John Martin <jmart07@gmail.com>
2020-02-11 21:17:15 -0500

    added characters_controller and seeds from old files

--- dnd-character-creator-api/app/controllers/characters_controller.rb
+++ dnd-character-creator-api/app/controllers/characters_controller.rb
@@ -1,74 +1,67 @@
 class CharactersController < ApplicationController
-  before_action :set_character, only: [:show, :edit, :update, :destroy]
+  # before_action :set_character
 
   # GET /characters
-  # GET /characters.json
   def index
+    p "Character index route accessed"
     @characters = Character.all
+    render json: @characters
   end
 
   # GET /characters/1
-  # GET /characters/1.json
   def show
-  end
-
-  # GET /characters/new
-  def new
-    @character = Character.new
-  end
-
-  # GET /characters/1/edit
-  def edit
+    p "Character show route accessed"
+    set_character
+    render json: @character
   end
 
   # POST /characters
-  # POST /characters.json
   def create
+    p "Character create route accessed"
     @character = Character.new(character_params)
 
-    respond_to do |format|
-      if @character.save
-        format.html { redirect_to @character, notice: 'Character was successfully created.' }
-        format.json { render :show, status: :created, location: @character }
-      else
-        format.html { render :new }
-        format.json { render json: @character.errors, status: :unprocessable_entity }
-      end
-    end
+    if @character.save
+      render json: @character, status: :created, location: @character
+    else
+      render json: @character.errors, status: :unprocessable_entity
   end
 
   # PATCH/PUT /characters/1
-  # PATCH/PUT /characters/1.json
   def update
-    respond_to do |format|
-      if @character.update(character_params)
-        format.html { redirect_to @character, notice: 'Character was successfully updated.' }
-        format.json { render :show, status: :ok, location: @character }
-      else
-        format.html { render :edit }
-        format.json { render json: @character.errors, status: :unprocessable_entity }
-      end
+    p "Character update route accessed"
+
+    if @character.update(character_params)
+      render json: @character
+    else
+      render json: @character.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /characters/1
-  # DELETE /characters/1.json
+  # DELETE /characters/1 -- returns character deleted
   def destroy
-    @character.destroy
-    respond_to do |format|
-      format.html { redirect_to characters_url, notice: 'Character was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    p "Character destroy route accessed"
+    render json: @character.destroy()
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_character
-      @character = Character.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def character_params
-      params.fetch(:character, {})
-    end
+  def set_character
+    @character = Character.find(params[:id])
+  end
+
+  def character_params
+    p "Params are..."
+    p params
+    # params.permit!
+    params.require(:character).permit(
+      :name,
+      :strength,
+      :dexterity,
+      :constitution,
+      :intelligence,
+      :wisdom,
+      :charisma,
+    )
+  end
+
 end
